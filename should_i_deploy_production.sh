940dc4ca2facd1975f5e1c385a13bd4a68082ead
Michael H. Oshita <ijinpublic+github@gmail.com>
2023-01-11 19:31:01 +0900

    ignore build step

---
+++ should_i_deploy_production.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+
+if [[ "$VERCEL_ENV" == "production" && ! `git diff HEAD^ HEAD --quiet CHANGELOG.md` ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+fi
